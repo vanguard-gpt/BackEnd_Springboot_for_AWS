9e1862bd59bf7912eebdf1a6737e114800f2000f
vanguard <vanguard09@naver.com>
2024-08-12 09:55:01 +0900

    controller +

--- src/main/java/com/restapi/emp/service/impl/DepartmentServiceImpl.java
+++ src/main/java/com/restapi/emp/service/impl/DepartmentServiceImpl.java
@@ -46,11 +46,10 @@ public class DepartmentServiceImpl implements DepartmentService {
 
     private Department getDepartment(Long departmentId) {
         String errMsg = String.format("Department is not exists with a given id: %s", departmentId);
-        Department department = departmentRepository.findById(departmentId)
+        return departmentRepository.findById(departmentId)
                 .orElseThrow(() ->
                         new ResourceNotFoundException(errMsg, HttpStatus.NOT_FOUND)
                 );
-        return department;
     }
 
     @Transactional(readOnly = true)
@@ -80,10 +79,7 @@ public class DepartmentServiceImpl implements DepartmentService {
 
     @Override
     public void deleteDepartment(Long departmentId) {
-        departmentRepository.findById(departmentId)
-                .orElseThrow(() ->
-                        new ResourceNotFoundException("Department is not exists with a given id: " + departmentId)
-                );
+        Department department = getDepartment(departmentId);
 
         departmentRepository.deleteById(departmentId);
     }
